879045f34290c6eaa8228ca507d30f8a16854967
SeanSanthosh53 <sean.santhosh.thomas@gmail.com>
2022-04-10 15:39:05 +0530

    Built better home screen, custom buttons with pop-up-hover effect

--- Android App/main.py
+++ Android App/main.py
@@ -1,10 +1,30 @@
-import kivy
 from kivymd.app import MDApp
 from kivy.lang import Builder
 
+from kivy.core.window import Window
+from kivy.properties import StringProperty
+from kivy.uix.screenmanager import Screen
+
+from kivymd.uix.card import MDCard
+from kivymd.uix.behaviors import HoverBehavior
+
+
+# The Main/Home Screen
+class HomeScreen(Screen):
+    pass
+
+
+# The Idea Log/Diary Screen
+class IdeaLogScreen(Screen):
+    pass
+
+
+class CustomIconButton(MDCard, HoverBehavior):
+    text = StringProperty()
+
 
 class TestApp(MDApp):
-    def build(self): 
+    def build(self):
         self.root = Builder.load_file("design.kv")
 
 
